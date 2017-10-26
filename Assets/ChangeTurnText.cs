9ce8625344c225d5960bae4de55b38060d717bc0
GerritsC <gerrits.c@husky.neu.edu>
2017-10-26 14:24:54 -0400

    added switchText for UI object

--- Assets/ChangeTurnText.cs
+++ Assets/ChangeTurnText.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ChangeTurnText : MonoBehaviour {
 
+
+    [SerializeField]
+    private Color yourturnColor;
+    [SerializeField]
+    private Color enemyturnColor;
+
+    [SerializeField]
+    private string yourturn = "Your turn";
+    [SerializeField]
+    private string enemyturn = "Enemy turn";
+
+    private Text text;
+
 	// Use this for initialization
 	void Start () {
-		
+        text = GetComponent<Text>();
+
+        text.text = yourturn;
+        text.color = yourturnColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switchText();
+        }
 	}
+
+    private void switchText()
+    {
+        if (text.text == yourturn)
+        {
+            text.text = enemyturn;
+            text.color = enemyturnColor;
+        }
+        else
+        {
+            text.text = yourturn;
+            text.color = yourturnColor;
+        }
+    }
 }

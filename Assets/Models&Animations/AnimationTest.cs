c9df44c565029a8c2c505c5a253ee5d98e0a0a97
JerryNie <nieshixin@gmail.com>
2017-10-29 22:08:01 -0400

    Move - Run animation blend tree, removed unneeded animation files

---
+++ Assets/Models&Animations/AnimationTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour {
+
+	Animator animator;
+
+	private string animState;
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.W)){
+				animator.SetFloat ("Speed", 1);
+			}
+		if(Input.GetKey(KeyCode.S)){
+			animator.SetFloat ("Speed", 0);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(Vector3.up * Time.deltaTime*50);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.down * Time.deltaTime*50);
+		}
+	
+	}
+
+}

bb9f9779270fd5d442c3ef25707b3c90bccf5929
svogel <svogel@ccs.neu.edu>
2018-03-09 18:53:11 -0500

    Added camera zoom with scroll wheel

---
+++ Assets/Scripts/CameraFollowBezier.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBezier : MonoBehaviour {
+
+	public BezierCurve curve = null;
+
+	private float currT = 1f;
+	public float scrollSpeed = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currT += Mathf.Clamp(Input.GetAxis("Mouse ScrollWheel") * -scrollSpeed, -1, 1);
+		var newPos = curve.GetPointAt (currT);
+		transform.position = newPos;
+		// Fix lookat
+		var lookat = curve.GetPointAt (currT - .1f);
+		transform.LookAt (curve[0].position);
+		transform.localRotation = Quaternion.Euler( new Vector3 (transform.localRotation.eulerAngles.x, 0, 0));
+	}
+}
